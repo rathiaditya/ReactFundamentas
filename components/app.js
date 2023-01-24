d248189fd9e8b6d4f193eefd7febf59938cb4857
Aditya Rathi <rathiaditya7@gmail.com>
2023-01-23 19:40:32 -0600

    Second commit

---
+++ components/app.js
@@ -0,0 +1,8 @@
+import Banner from "./banner";
+
+const App = () => {
+  return <Banner />
+
+};
+
+export default App;
---
+++ components/banner.js
@@ -0,0 +1,14 @@
+const Banner = () => {
+  return (
+    <header>
+      <div>
+        <img src="./GloboLogo.png" alt="logo"></img>
+      </div>
+      <div>
+        Providing houses all over the world - USA
+      </div>
+    </header>
+  );
+};
+
+export default Banner;
